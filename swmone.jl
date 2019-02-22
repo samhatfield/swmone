98b96d431f889976761f30b0bf30aab0c711efc6
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2019-02-22 15:52:28 +0000

    Removed MPI and plotting features

--- swmone.jl
+++ swmone.jl
@@ -1,6 +1,3 @@
-using PyPlot
-using MPI
-using Printf
 
 """Gradient function. 2nd-order centred."""
 function ∂x!(du::AbstractVector,u::AbstractVector)
@@ -43,7 +40,7 @@ function rhs!(du,dη,u,η,h,u²,h_u,u²_h,dudx,p,dpdx,U,dUdx,t)
 
     # momentum equation
     ∂x!(dpdx,p)
-    @views du[2:end-1] .= dpdx .+ F(x_up,t)./h_u[2:end]
+    @views du[2:end-1] .= dpdx .+ F(x_u,t)./h_u[2:end]
 
     # continuity equation
     @views U .= u[1:end-1].*h_u         # volume flux
@@ -51,54 +48,26 @@ function rhs!(du,dη,u,η,h,u²,h_u,u²_h,dudx,p,dpdx,U,dUdx,t)
     @views dη[2:end-1] .= -dUdx
 end
 
-""""Size of the subdomain per process."""
-function subdomain_size(N::Int,size::Int)
-    if mod(N,size) == 0
-        return N ÷ size
-    else
-        throw(error("Domain size must be divisible by number of processes."))
-    end
-end
 
-"""Rank of the neighbouring processes with wrap-around behaviour (periodic boundary conditions)."""
-function neighbours(size::Int,rank::Int)
-    # left, right neighbour
-    lnb,rnb = mod(rank-1,size),mod(rank+1,size)
-end
 
 """Add halo to the left and right of each subdomain for ghost point communication."""
-function add_halo(u::AbstractVector,η::AbstractVector,rmsg::AbstractVector,lmsg::AbstractVector)
+function add_halo(u::AbstractVector,η::AbstractVector)
     u = cat(0.,u,0.,dims=1)
     η = cat(0.,η,0.,dims=1)
 
-    ghost_points!(u,η,rmsg,lmsg)
+    ghost_points!(u,η)
     return u,η
 end
 
 """Propagate the boundary conditions (BC) via ghost point communication.
 For multiple processes, BC of subdomains are communicated via MPI."""
-function ghost_points!(u::AbstractVector,η::AbstractVector,rmsg::AbstractVector,lmsg::AbstractVector)
-    if size == 1
-        # sequential, periodic BC
-        u[1] = u[end-1]
-        u[end] = u[2]
+function ghost_points!(u::AbstractVector,η::AbstractVector)
+    # sequential, periodic BC
+    u[1] = u[end-1]
+    u[end] = u[2]
 
-        η[1] = η[end-1]
-        η[end] = η[2]
-
-    else
-        # parallel with communication
-        MPI.Isend([u[2],η[2]],lnb,rank+10,comm)
-        MPI.Isend([u[end-1],η[end-1]],rnb,rank+20,comm)
-
-        rreq = MPI.Irecv!(rmsg,rnb,rnb+10,comm)
-        lreq = MPI.Irecv!(lmsg,lnb,lnb+20,comm)
-
-        MPI.Waitall!([rreq,lreq])
-
-        u[end],η[end] = rmsg
-        u[1],η[1] = lmsg
-    end
+    η[1] = η[end-1]
+    η[end] = η[2]
 end
 
 """Preallocate intermediate variables used in the right-hand side computation."""
@@ -124,13 +93,8 @@ end
 
 """4th order Runge-Kutta time integration."""
 function time_integration(Nt,u,η)
-
-    # preallocate for parallel: message from right, left
-    rmsg = Array{Float64}(undef,2)
-    lmsg = Array{Float64}(undef,2)
-
     # pre-allocate memory
-    u,η = add_halo(u,η,rmsg,lmsg)
+    u,η = add_halo(u,η)
     u0,η0,u1,η1,du,dη,h,u²,h_u,u²_h,dudx,p,dpdx,U,dUdx = preallocate(u,η)
 
     t = 0.
@@ -145,13 +109,13 @@ function time_integration(Nt,u,η)
 
     for i = 1:Nt
 
-        ghost_points!(u,η,rmsg,lmsg)
+        ghost_points!(u,η)
         u1 .= u
         η1 .= η
 
         for RKi = 1:4
             if RKi > 1
-                ghost_points!(u1,η1,rmsg,lmsg)
+                ghost_points!(u1,η1)
             end
 
             rhs!(du,dη,u1,η1,h,u²,h_u,u²_h,dudx,p,dpdx,U,dUdx,t)
@@ -179,11 +143,7 @@ function time_integration(Nt,u,η)
     u_out,η_out
 end
 
-MPI.Init()
-comm = MPI.COMM_WORLD
 
-const rank = MPI.Comm_rank(comm)
-const size = MPI.Comm_size(comm)
 
 const g = 10.
 const H = 10.
@@ -205,57 +165,9 @@ const RKβ = [0.5,0.5,1.]
 const x_h = dx/2:dx:Lx
 const x_u = dx:dx:Lx        # no u-point at x=0 but at x=L (periodicity)
 
-# subdomains for parallel
-const Np = subdomain_size(N,size)
-const x_up = x_u[(rank*Np+1):(rank+1)*Np]
-const lnb,rnb = neighbours(size,rank)
-
 # initial conditions
-u_ini = fill(0.,Np)
-η_ini = fill(0.,Np)
+u_ini = fill(0.,N)
+η_ini = fill(0.,N)
 
 u,η = time_integration(Nt,u_ini,η_ini)
 
-# receive solutions from all processors
-ηall = MPI.Gather(η,0,comm)
-if rank == 0
-    ηall = reshape(ηall,Nt+1,N)
-end
-
-MPI.Barrier(comm)
-MPI.Finalize()
-
-## PLOTTING
-ioff()
-if rank == 0
-    fig,ax = subplots(figsize=(6,3))
-
-    l1, = ax[:plot](x_h,ηall[1,:],lw=2)
-    ax[:set_xlim](0,1)
-    ax[:set_ylim](-1.2,1.7)
-    ax[:set_yticks]([-1,0,1])
-    ax[:set_xticks]([0,1])
-    ax[:set_xlabel]("x")
-    ax[:set_ylabel]("η")
-    tight_layout()
-
-    ax[:plot]([0,.504],[1.3,1.3],"C1")
-    ax[:plot]([0.996,1.0],[1.3,1.3],"C1")
-
-    ax[:plot]([0.496,1],[1.33,1.33],"C2")
-    ax[:plot]([0.0,0.004],[1.33,1.33],"C2")
-
-    ax[:text](0.1,1.4,"Process 0")
-    ax[:text](0.8,1.4,"Process 1")
-
-    #global iframe = 0
-    for it = 2:2:1000
-        pause(0.00001)
-        l1[:set_data](x_h,ηall[it,:])
-        #savefig("/home/kloewer/julia/vid/waveframes/frame"*Printf.@sprintf("%04d",iframe)*".png",dpi=75)
-        #iframe += 1
-    end
-
-    pause(2)
-    close(fig)
-end
